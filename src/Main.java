fe64004e653e69104b10b7fc57dc7bde5e40da9e
TIME <opout777@yandex.ru>
2023-01-19 14:29:03 +0300

    Task 7

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         task4();
         task5();
         task6();
+        task7();
     }
 
     public static void task1() {
@@ -31,5 +32,9 @@ public class Main {
     public static void task6() {
         System.out.println("Задача 6");
         for (int i = 7; i <= 98; i+= 7) {System.out.println(i);}
+    }
+    public static void task7() {
+        System.out.println("Задача 7");
+        for (int i = 1; i <= 512; i*= 2) {System.out.println(i);}
     }
 }
